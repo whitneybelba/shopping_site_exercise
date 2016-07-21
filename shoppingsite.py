6547977a5a29f78454eb9893c40e31f225d45a38
Hackbright Student <info@hackbrightacademy.com>
2016-07-21 13:03:39 -0700

    add melon to cart and flash message

--- shoppingsite.py
+++ shoppingsite.py
@@ -7,11 +7,11 @@ Authors: Joel Burton, Christian Fernandez, Meggie Mahnken.
 """
 
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
 import jinja2
 
 import melons
-
 
 app = Flask(__name__)
 
@@ -50,7 +50,7 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id(59)
+    melon = melons.get_by_id(melon_id)
     print melon
     return render_template("melon_details.html",
                            display_melon=melon)
@@ -70,7 +70,7 @@ def shopping_cart():
     #   - keep track of the total amt ordered for a melon-type
     #   - keep track of the total amt of the entire order
     # - hand to the template the total order cost and the list of melon types
-
+    print session
     return render_template("cart.html")
 
 
@@ -82,13 +82,14 @@ def add_to_cart(id):
     page and display a confirmation message: 'Successfully added to cart'.
     """
 
+    session.setdefault('cart', []).append(id)
     # TODO: Finish shopping cart functionality
 
     # The logic here should be something like:
     #
     # - add the id of the melon they bought to the cart in the session
-
-    return "Oops! This needs to be implemented!"
+    flash("The melon was successfully added to the cart!")
+    return redirect("/cart")
 
 
 @app.route("/login", methods=["GET"])
@@ -122,5 +123,10 @@ def checkout():
     return redirect("/melons")
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.debug = True
+    DebugToolbarExtension(app)
+    app.run()
